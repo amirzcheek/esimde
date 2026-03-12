3479b61988350cae9de17a2e9ab6a37129caa0f2
amirzcheek <amirzilyasov@gmail.com>
2026-03-13 00:39:14 +0500

    add: воспоминания

--- backend/app/api/v1/endpoints/memories.py
+++ backend/app/api/v1/endpoints/memories.py
@@ -5,7 +5,8 @@ from sqlalchemy import select
 from typing import Optional
 
 from app.core.config import settings
-from app.core.deps import get_db, get_current_user
+from app.core.database import get_db
+from app.core.security import get_current_user
 from app.models.user import User
 from app.models.models import Memory
 
